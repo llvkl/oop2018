c04ee4ec19e34e49a177f4de05e95a7c8d881557
llvkl <lllllvkl@gmail.com>
2018-09-18 13:52:51 +0700

    meow1

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,13 +3,47 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
+    private String name;
+    private String id;
+    private String group;
+    private String email;
+    public void setName(String name) {
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setId(String id) {
+        this.id=id;
+    }
+    public String getId(){
+        return id;
+    }
+    void setGroup(String group) {
+        this.group=group;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public void setEmail(String email) {
+        this.email=email;
+    }
+    public String getEmail(){
+
+        return email;
+    }
 
     // TODO: khai báo các phương thức getter, setter cho Student
 
     /**
      * Constructor 1
+     }
      */
     Student() {
+    name = "Student";
+    id = "000";
+    group = "INT22041";
+    email = "uet@vnu.edu.vn";
         // TODO:
     }
 
@@ -20,6 +54,11 @@ public class Student {
      * @param em
      */
     Student(String n, String sid, String em) {
+        this.name= n;
+        this.id=sid;
+        this.email=em;
+        group = "INT22041";
+
         // TODO:
     }
 
@@ -29,10 +68,15 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        name=s.name;
+        id=s.id;
+        group=s.group;
+        email=s.email;
     }
 
     String getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        System.out.println(name+"\n"+id+"\n"+group+"\n"+email);
+        return null;
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,23 +1,53 @@
 package week1;
 
 public class StudentManagement {
-
-    // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-
+        int n=0;
+        Student[] students = new Student[100];
+        // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+        public void Student (Student s) {
+            while (n <= 100) {
+                students[n] = s;
+                n++;
+            }
+        }
     public boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
-    }
+        return (s1.getGroup()==s2.getGroup());
+
+        }
 
     void studentsByGroup() {
         // TODO:
-    }
 
-    void removeStudent(String id) {
-        // TODO:
     }
-
-    public static void main(String[] args) {
+//    void removeStudent(String id) {
         // TODO:
+//    }
+
+    public static void main (String[] args) {
+        // TODO:
+        Student doanxem = new Student();
+        doanxem.setName("Doan xem 1");
+        doanxem.setId("Doan xem 2");
+        doanxem.setGroup("Doan xem 3");
+        doanxem.setEmail("Doan xem 4");
+        doanxem.getInfo();
+        Student a = new Student();
+        a.getInfo();
+        Student b = new Student("DOAN XEM 1","DOAN XEM 2","DOAN XEM 3");
+        b.getInfo();
+        Student c = new Student(b);
+        c.getInfo();
+        Student dx1 = new Student("dx1", "111","meow1");
+        dx1.setGroup("INT22041");
+        Student dx2 = new Student("dx2", "222","meow2");
+        dx2.setGroup("INT22041");
+        Student dx3 = new Student("dx3", "333","meow3");
+        dx3.setGroup("INT22042");
+        StudentManagement gopro = new StudentManagement();
+        System.out.println(gopro.sameGroup(dx1,dx2));
+        System.out.println(gopro.sameGroup(dx2,dx3));
+        System.out.println(gopro.sameGroup(dx1,dx3));
+
     }
 }

3412fe1596cc84e71b569851f79b7f6110f8a662
CCNE <lllllvkl@gmail.com>
2018-09-26 16:30:41 +0700

    ahii

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,14 +1,23 @@
 package week2.task1;
+import java.util.Scanner;
 
 public class Task1 {
 
     public static int gcd(int a, int b) {
-        // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        if (a==0) return b;
+        return gcd(b%a,a);
     }
 
     public static int fibonacci(int n) {
-        // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+        if (n == 0) return 0;
+        else if (n == 1) return 1;
+        else{
+        return fibonacci(n-1) + fibonacci(n-2);
+    }}
+
+    public static void main(String[] args) {
+        System.out.println("UCLN"+gcd(40,60));
+        System.out.println(fibonacci(7));
     }
 }
+
--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -3,28 +3,84 @@ package week2.task2;
 public class Fraction {
 
     // TODO: khai báo các thuộc tính
-
+    private int numerator;
+    private int denominator;
     public Fraction(int numerator, int denominator) {
-        // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
+
+        this.numerator=numerator;
+        this.denominator=denominator;
     }
 
     public Fraction add(Fraction other) {
+        Fraction add = new Fraction(this.numerator*other.denominator + other.numerator*this.denominator, this.denominator*other.denominator);
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        return add;
     }
 
     public Fraction subtract(Fraction other) {
+        Fraction subtract = new Fraction(this.numerator*other.denominator - other.numerator*this.denominator, this.denominator*other.denominator);
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        return subtract;
     }
 
     public Fraction multiply(Fraction other) {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction multiply = new Fraction(this.numerator*other.numerator, this.denominator*other.denominator);
+        toigian();
+        return multiply;
     }
 
     public Fraction divide(Fraction other) {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction divine = new Fraction(this.numerator*other.denominator, this.denominator*other.numerator);
+        return divine;
     }
+    public Fraction toigian(){
+        Fraction a = new Fraction(this.numerator, this.denominator);
+        if(this.numerator == 0){
+            a.numerator = 0;
+            a.denominator = 1;
+        }
+        else if(this.numerator >= this.denominator){
+            for(int i=this.denominator; i>1; i--){
+                if(this.denominator % i == 0){
+                    a.numerator = this.numerator / i;
+                    a.denominator = this.denominator / i;
+                    break;
+                }
+            }
+        }
+        else{
+            for(int i=this.numerator; i>1; i--){
+                if(this.numerator % i == 0 && this.denominator % i == 0){
+                    a.numerator = this.numerator / i;
+                    a.denominator = this.denominator / i;
+                    break;
+                }
+            }
+        }
+        return a;
+    }
+
+    public boolean equals(Fraction a){
+        return (this.toigian().numerator == a.toigian().numerator && this.toigian().denominator == a.toigian().denominator);
+    }
+    public void println(){
+        System.out.println(this.numerator + "/" + this.denominator);
+    }
+    public static void main (String[] args){
+        Fraction a = new Fraction(10,20);
+        Fraction b = new Fraction(11,12);
+        System.out.println("a+b");
+        a.add(b).println();
+        System.out.println("a-b");
+        a.multiply(b).toigian().println();
+        System.out.println("a*b");
+        a.subtract(b).toigian().println();
+        System.out.println("a/b");
+        a.divide(b).println();
+        System.out.println(a.equals(b));
+    }
+
+
 }

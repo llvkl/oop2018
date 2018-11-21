eb5a6a5bf751ca3b6bd031715fb91cd64bf87cb3
CCNE <lllllvkl@gmail.com>
2018-11-21 16:27:09 +0700

    123

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,27 @@
 package week11;
 
+
 public class Task1 {
+    public static <A extends Comparable> void sapxep(A[] array){
+        A temp;
+        for(int i=0;i<array.length;i++){
+            for(int j=i+1;j<array.length;j++){
+            if(array[i].compareTo(array[j])>0){
+                temp=array[i];
+                array[i]=array[j];
+                array[j]=temp;
+            }
+            }}
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]+" ");}
+        System.out.println();
+    }
+    public static void main(String[] args){
+        Integer[] integers= { 6,9,1,45,2,4};
+        sapxep(integers);
+        Double[] doubles = {6.9,3.6,46.2,5.1,8.4};
+        sapxep(doubles);
+        Character[] characters= {'f','e','g','a','v','y'};
+        sapxep(characters);
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,23 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task2 {
+    public static <A extends Comparable> void getmax(ArrayList<A> array){
+        A max = array.get(0);
+        for(int i=0;i<array.size();i++){
+            if(array.get(i).compareTo(max)>0){
+                max = array.get(i);
+            }
+    }
+        System.out.println(max);
+}
+    public static void main(String[] args){
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(5);
+        integers.add(23);
+        integers.add(69);
+        integers.add(31);
+        getmax(integers);
+    }
 }
